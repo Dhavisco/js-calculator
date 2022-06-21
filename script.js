52b844b4d7b9f48f5043dadae40569584babd363
David Ayomide Olaniyi <63981998+Dhavisco@users.noreply.github.com>
2022-06-21 09:24:16 +0100

    script

---
+++ script.js
@@ -0,0 +1,33 @@
+// program for a simple arithmetic calculator
+
+// take the operator input
+const operator = prompt('Enter operator ( either +, -, *, /, or % ): ');
+
+// take the operand input
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
+
+let result;
+
+// using if...else if... else
+if (operator == '+') {
+    result = number1 + number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else if(operator == '/') {
+    result = number1 / number2;
+}
+else if (operator == '%') {
+    result = number1 % number2;
+}
+else {
+    console.log("enter a valid operator");
+}
+
+// display the result
+console.log(`${number1} ${operator} ${number2} = ${result}`);
